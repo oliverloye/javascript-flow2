aeceeb0069add8f37608810f73e93dd0a34a01a8
Oliver <oliverloye@me.com>
2019-03-21 14:50:14 +0100

    some more tests

--- expressExercise/test/testCalc.js
+++ expressExercise/test/testCalc.js
@@ -13,29 +13,43 @@ describe("Calculator API", function () {
             const result = calc.add(4, 3);
             expect(result).to.be.equal(7);
         })
-    })
+        it(" Sub 5 - 10. Should return -5", function () {
+            const result = calc.sub(5, 10);
+            expect(result).to.be.equal(-5);
+        })
+        it(" Divide 10 / 2. Should return 5", function () {
+            const result = calc.divide(10, 2);
+            expect(result).to.be.equal(5);
+        })
+    });
 
     describe("testing the REST Calc api", function () {
+
         before(function (done) {
-            calc.calcServer(PORT, function (s) {
-                server = s;
+            calc.calcServer(PORT, function (testServer) {
+                server = testServer;
                 done();
             })
-        })
+        });
 
         it("Add 4 + 3 should return 7", async function () {
             const res = await fetch(URL + "add/4/3").then(r => r.text());
             expect(res).to.be.equal('7');
-        })
-       
-        it("Sub 3-2 should return 1", async function (){
+        });
+
+        it("Sub 3 - 2 should return 1", async function () {
             const res = await fetch(URL + "sub/3/2").then(r => r.text())
             expect(res).to.be.equal('1');
-        })
+        });
+
+        it("Divide 10 / 2 should return 5", async function () {
+            const res = await fetch(URL + "divide/10/2").then(r => r.text())
+            expect(res).to.be.equal('5');
+        });
 
         after(function () {
             server.close();
         })
-    
+
     })
 })
